7de45e7246804f4d7e03055eff118bdb57a84522
plungerman <plungerman@gmail.com>
2018-03-06 10:54:24 -0600

    added logic such that signals are ignored when loading data via fixtures

--- djtwilio/core/models.py
+++ djtwilio/core/models.py
@@ -52,10 +52,10 @@ class Profile(models.Model):
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    if created:
+    if created and not kwargs.get('raw', False):
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
+    if not kwargs.get('raw', False):
+        instance.profile.save()
