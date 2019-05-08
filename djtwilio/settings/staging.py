6d8f2cd4f1442aa738027f0c5fea4359be4c2111
plungerman <plungerman@gmail.com>
2019-05-08 14:23:58 -0500

    staging settings

--- djtwilio/settings/staging.py
+++ djtwilio/settings/staging.py
@@ -1,6 +1,13 @@
 from .local import *
 
+#from djzbar.settings import INFORMIX_EARL_PROD as INFORMIX_EARL
 from djzbar.settings import INFORMIX_EARL_TEST as INFORMIX_EARL
 
+ALLOWED_HOSTS =  [
+    'localhost','127.0.0.1','psm.carthage.edu','hygiea.carthage.edu',
+    'www.carthage.edu'
+]
+#DEBUG = False
 DEBUG = True
-ROOT_URL = '/apps/twilio/'
+ROOT_URL = '/apps/twilio'
+LOGIN_REDIRECT_URL = ROOT_URL
