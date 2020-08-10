7b16cf2c6aacfe402e0b21fadb893ec921a202a7
plungerman <plungerman@gmail.com>
2020-08-10 17:41:23 -0500

    moved to using a url shortening feature

--- djtwilio/redpanda/students.py
+++ djtwilio/redpanda/students.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import json
 import os
+import requests
 import sys
 
 # env
@@ -13,9 +15,7 @@ django.setup()
 from django.conf import settings
 from djimix.core.database import get_connection
 from djimix.core.database import xsql
-from djimix.core.encryption import encrypt
 from djtools.utils.mail import send_mail
-from djtwilio.apps.sms.models import Message
 from djtwilio.core.models import Sender
 from djtwilio.core.utils import send_message
 from twilio.rest import Client
@@ -61,11 +61,17 @@ def main():
             sql = "SELECT * FROM fwk_user WHERE HostID like '%{}'".format(student[0])
             row = xsql(sql, mssql_cnxn).fetchone()
             if row:
-                earl = 'https://{0}{1}?uid={2}'.format(
+                earl = 'https://{0}{1}{2}?uid={3}'.format(
                     settings.REDPANDA_SERVER_URL,
                     settings.REDPANDA_ROOT_URL,
-                    encrypt(row[0]),
+                    settings.REDPANDA_SHORT_URL_API,
+                    row[0],
                 )
+                print(earl)
+                response = requests.get(earl)
+                jason_data = json.loads(response.text)
+                earl = jason_data['lynx']
+                print(earl)
                 # send an SMS or an email
                 #if student.mobile:
                 if student[6]:
