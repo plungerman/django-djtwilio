9fdf75dc27c64d0ddfdf383aa89fc06a724ddcd7
plungerman <plungerman@gmail.com>
2021-04-08 13:26:16 -0500

    migrating from python 2.7 to 3.6 and from django 1.11.x to 2.2.x

--- djtwilio/core/urls.py
+++ djtwilio/core/urls.py
@@ -9,7 +9,9 @@ from django.urls import path
 from django.urls import reverse_lazy
 from django.views.generic import RedirectView
 from django.views.generic import TemplateView
+
 from djauth.views import loggedout
+
 from djtwilio.core import views
 
 admin.autodiscover()
@@ -21,52 +23,51 @@ handler500 = 'djtools.views.errors.server_error'
 
 urlpatterns = [
     # django admin and loginas
-    url(r'^rocinante/', include(admin.site.urls)),
-    url(r'^rocinante/', include('loginas.urls')),
+    path('rocinante/', include(admin.site.urls)),
+    path('rocinante/', include('loginas.urls')),
     # admin honeypot
-    url(r'^admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     # auth
-    url(
-        r'^accounts/login/$',
+    path(
+        'accounts/login/',
         auth_views.login,
         {'template_name': 'accounts/login.html'},
         name='auth_login',
     ),
-    url(
-        r'^accounts/logout/$',
+    path(
+        'accounts/logout/',
         auth_views.logout,
         {'next_page': reverse_lazy('auth_loggedout')},
         name='auth_logout',
     ),
-    url(
-        r'^accounts/loggedout/$',
+    path(
+        'accounts/loggedout/',
         loggedout,
         {'template_name': 'accounts/logged_out.html'},
         name='auth_loggedout',
     ),
-    url(
+    path(
         r'^accounts/profile/sender/manager/$',
         views.sender_manager,
         name='sender_manager',
     ),
-    url(
+    path(
         r'^accounts/profile/sender/manager/(?P<sid>\w+)/(?P<action>[-\w]+)/$',
         views.sender_manager,
         name='sender_update',
     ),
-    url(r'^accounts/profile/$', views.user_profile, name='user_profile'),
-    url(r'^accounts/$', RedirectView.as_view(url=reverse_lazy('auth_login'))),
-    url(
+    path(r'^accounts/profile/$', views.user_profile, name='user_profile'),
+    path(r'^accounts/$', RedirectView.as_view(url=reverse_lazy('auth_login'))),
+    path(
         r'^denied/$',
         TemplateView.as_view(
             template_name='denied.html'
         ),
         name='access_denied',
     ),
-    url(r'^core/student/list/$', views.student_list, name='student_list'),
+    path(r'^core/student/list/$', views.student_list, name='student_list'),
     # apps could have its own urls.py file eventually
-    url(r'^sms/', include('djtwilio.apps.sms.urls')),
+    path(r'^sms/', include('djtwilio.apps.sms.urls')),
     # redirect home request to sms for now
-    url(r'^$', RedirectView.as_view(url=reverse_lazy('sms_send_form'))),
+    path(r'^$', RedirectView.as_view(url=reverse_lazy('sms_send_form'))),
 ]
-urlpatterns += url("admin/", include('loginas.urls')),
--- djtwilio/settings/staging.py
+++
@@ -1,8 +0,0 @@
-from .local import *
-from djzbar.settings import INFORMIX_EARL_TEST as INFORMIX_EARL
-
-
-ALLOWED_HOSTS =  [
-    'localhost', '127.0.0.1', 'hygiea.carthage.edu', 'www.carthage.edu',
-]
-DEBUG = True
