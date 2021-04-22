2b57fdd5ea69ee17254a617367ad5079d75358bb
plungerman <plungerman@gmail.com>
2021-04-22 09:31:03 -0500

    we now obtain the Sender() data model instance and the twilio Client() instance in the send_message() function and check for a valid Sender instance

--- djtwilio/core/utils.py
+++ djtwilio/core/utils.py
@@ -1,68 +1,78 @@
 # -*- coding: utf-8 -*-
 
+import logging
 import re
 
 from django.conf import settings
 from django.urls import reverse
-
 from djimix.core.encryption import encrypt
-from twilio.base.exceptions import TwilioRestException
-
 from djtwilio.apps.sms.models import Message
 from djtwilio.apps.sms.models import Status
+from djtwilio.core.models import Sender
+from twilio.base.exceptions import TwilioRestException
+from twilio.rest import Client
+
+logger = logging.getLogger(__name__)
 
 
-def send_message(client, sender, recipient, body, cid, callback=False, bulk=None, doc=None):
+def send_message(sid, recipient, body, cid, callback=False, bulk=None, doc=None):
     """Send a message through the twilio api."""
-    # this seems like overkill but you never know
-    if bulk:
-        phrum = sender.messaging_service_sid
-    else:
-        phrum = sender.phone
-        if not phrum:
+    sender = Sender.objects.filter(pk=sid).first()
+    if sender:
+        client = Client(sender.account.sid, sender.account.token)
+        # this seems like overkill but you never know
+        if bulk:
             phrum = sender.messaging_service_sid
-    try:
-        # create Status object
-        status = Status.objects.create()
-        # create Message object before API call
-        recipient = re.sub('[^A-Za-z0-9]+', '', recipient)
-        message = Message.objects.create(
-            messenger=sender,
-            recipient=recipient,
-            student_number=cid,
-            body=body,
-            bulk=bulk,
-            status=status,
-            phile=doc,
-        )
-        if not callback:
-            callback = 'https://{0}{1}'.format(
-                settings.SERVER_URL,
-                reverse(
-                    'sms_status_callback',
-                    args=[encrypt(str(message.id))],
-                ),
+        else:
+            phrum = sender.phone
+            if not phrum:
+                phrum = sender.messaging_service_sid
+        logger.debug('{0}: {1}'.format(recipient, cid))
+        try:
+            # create Status object
+            status = Status.objects.create()
+            # create Message object before API call
+            recipient = re.sub('[^A-Za-z0-9]+', '', recipient)
+            message = Message.objects.create(
+                messenger=sender,
+                recipient=recipient,
+                student_number=cid,
+                body=body,
+                bulk=bulk,
+                status=status,
+                phile=doc,
             )
-        media_url = []
-        if doc:
-            media_url = [
-                'https://{0}/{1}'.format(settings.SERVER_URL, doc.phile.url),
-            ]
-        # use parentheses around body value to prevent extra whitespace
-        apicall = client.messages.create(
-            to=recipient,
-            from_=phrum,
-            body=(body),
-            status_callback=callback,
-            media_url=media_url,
-        )
-        status.SmsSid = apicall.sid
-        status.MessageSid = apicall.sid
-        status.SmsStatus = apicall.status
-        status.MessageStatus = apicall.status
-        status.save()
-    except TwilioRestException as error:
-        apicall = error
+            if not callback:
+                callback = 'https://{0}{1}'.format(
+                    settings.SERVER_URL,
+                    reverse(
+                        'sms_status_callback',
+                        args=[encrypt(str(message.id))],
+                    ),
+                )
+            media_url = []
+            if doc:
+                media_url = [
+                    'https://{0}/{1}'.format(settings.SERVER_URL, doc.phile.url),
+                ]
+            # use parentheses around body value to prevent extra whitespace
+            apicall = client.messages.create(
+                to=recipient,
+                from_=phrum,
+                body=(body),
+                status_callback=callback,
+                media_url=media_url,
+            )
+            status.SmsSid = apicall.sid
+            status.MessageSid = apicall.sid
+            status.SmsStatus = apicall.status
+            status.MessageStatus = apicall.status
+            status.save()
+        except TwilioRestException as error:
+            apicall = error
+            message = False
+    else:
+        apicall = 'Failed to fetch sender with ID: {0}'.format(sid)
         message = False
 
     return {'message': message, 'response': apicall}
