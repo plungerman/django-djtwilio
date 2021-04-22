2e8b5e1d0df0feb340cc974602bbd1c0771df2b5
plungerman <plungerman@gmail.com>
2021-04-22 09:31:37 -0500

    maquette for scheduled tasks

---
+++ djtwilio/bin/shed.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import django
+import os
+import sys
+from datetime import datetime
+from datetime import timedelta
+
+django.setup()
+
+from django.conf import settings
+from django.utils import timezone
+from django_q.models import Schedule
+from django_q.tasks import schedule
+
+# env
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djtwilio.settings.shell")
+
+
+def main():
+    """Testing task queue."""
+    body = 'La soledad es la gran talladora del espíritu.'
+    sender = settings.TWILIO_TEST_SENDER_ID
+    print(settings.TWILIO_TEST_SENDER_ID)
+    to = settings.TWILIO_TEST_PHONE_TO
+    print(to)
+    cid = settings.TWILIO_TEST_COLLEGE_ID
+    print(cid)
+    now = datetime.now()
+    next_run = timezone.now() + timedelta(minutes=1)
+    sched = schedule(
+        'djtwilio.core.utils.send_message',
+        sender,
+        to,
+        body,
+        cid,
+        schedule_type=Schedule.ONCE,
+        next_run=next_run,
+        repeats='1',
+        name='lorca: {0}'.format(now),
+    )
+    print(sched.__dict__)
+
+
+if __name__ == "__main__":
+
+    sys.exit(main())
